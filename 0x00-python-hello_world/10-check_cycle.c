eeb2377f02542a8f60f194c4b468ddf5a970d6bb
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-10-30 07:47:02 -0700

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if a singly lined list has a cycle in it
+ * @list: the list to check into
+ * Return: 0 if there is no cycle, and 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+}
