f8c23356103a92e68e353aecd2dd376349acca1f
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-10-31 11:27:24 -0700

    Task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -29,15 +29,13 @@ listint_t *insert_node(listint_t **head, int number)
 	while (current && current->next)
 	{
 		if (current->next->n > number)
-		{
-			before = current;
-			current = current->next;
 			break;
-		}
 		current = current->next;
 		check = 1;
 	}
 
+	before = current;
+	current = current->next;
 	if (check)
 	{
 		before->next = new;
