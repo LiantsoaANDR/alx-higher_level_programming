117ddfa5b671d54266933ad6adf4e1138660e47b
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-10-31 08:00:40 -0700

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
+    i = 0
     while i < len(str):
         c = ord(str[i])
         if c > 96 and c < 123:
-            C = c - 32
-            str[i] = chr(C)
+            c_to_p = chr(c - 32)
+        else:
+            c_to_p = str[i]
+        print("{}".format(c_to_p), end="")
         i += 1
+    print("")
