cdc7d0a2d4b468fd1003d7cb64446ac800531571
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-10-31 12:08:30 -0700

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,9 +3,8 @@ def remove_char_at(str, n):
     new_str = ""
     i = 0
     while i < len(str):
-        if i == n:
-            continue
-        new_str += str[i]
+        if i != n:
+            new_str += str[i]
         i += 1
 
     return new_str
