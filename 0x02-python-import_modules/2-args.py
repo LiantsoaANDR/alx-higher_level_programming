1859328ae40f13bb6a6835ddfadf7fe1091a80c4
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-02 09:07:05 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    import sys
 
-import sys
+    i = len(sys.argv) - 1
+    print("{} argument".format(i), end="")
+    if i > 1:
+        print("s", end="")
+    if len(sys.argv) < 2:
+        print(".")
+    else:
+        print(":")
 
-i = len(sys.argv) - 1
-print("{} argument".format(i), end="")
-if i > 1:
-    print("s", end="")
-if len(sys.argv) < 2:
-    print(".")
-else:
-    print(":")
-
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
