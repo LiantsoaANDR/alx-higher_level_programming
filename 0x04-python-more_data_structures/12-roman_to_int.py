148d0f7931f3c5b4f60587d949895a0fbd102038
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-08 08:07:15 -0800

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    nbr = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    past = 0
+
+    for i in range(len(roman_string) - 1, -1, -1):
+        if nbr[roman_string[i]] < past:
+            res -= nbr[roman_string[i]]
+        else:
+            res += nbr[roman_string[i]]
+        past = nbr[roman_string[i]]
+
+    return res
