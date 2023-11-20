de09d3473e996b546d996fc25fc9311a455c65af
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-20 10:14:29 -0800

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             nbr += 1
-        except:
+        except IndexError:
             continue
     print()
 
