5586af8b6622f563378ed3932a82532e9823dac6
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-11-27 12:37:27 -0800

    Task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module ofr text indentation """
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text: the text that needs the indentations
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    indentation = ['.', '?', ':']
+    flag = 0
+    for char in text:
+        if flag == 1:
+            flag = 0
+            continue
+        if char not in indentation:
+            print(char, end="")
+        else:
+            print("{}\n".format(char))
+            flag = 1
