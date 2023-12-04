73d73c0af903569eefa6cbeeb1a329bb17d15605
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-04 10:35:08 -0800

    Task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that check the instance"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is exactly an instance of a specified class
+
+    Args:
+        obj: the object to check
+        a_class: the specified class
+
+    Return: True if it is true, False otherwise
+    """
+    return (isinstance(obj, a_class))
