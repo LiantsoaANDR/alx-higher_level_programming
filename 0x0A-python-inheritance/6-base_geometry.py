e0b12aabd8eb941c388e6d440cbe63587b6699d9
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-04 11:49:28 -0800

    Tsak 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
