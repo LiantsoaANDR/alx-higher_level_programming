5383098817363534013f050da7aa3b92515c4a64
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-04 12:16:13 -0800

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for a class BaseGeometry (based on 6-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Check if value is an integer and is > 0
+
+        Args:
+            name: a name, as a string, that has a value
+            value: the value, should be an integer
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
