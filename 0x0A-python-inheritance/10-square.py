cf2b22da1d374744fa255103d067d823206e9222
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-04 12:59:31 -0800

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for Square,  that inherits from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines a square
+
+    Attributes:
+        width: the width of the rectangle
+        height: the height of the rectangle
+    """
+    def __init__(self, size):
+        """
+        Initialize the square
+
+        Args:
+            size: the size of the square
+        """
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
