6ff228aa2beca718dabb91030df62d2cfb7d1c81
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-05 12:12:43 -0800

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Give a representation of a Pascal's triange of n
+
+    Args:
+        n: an int, representing the rows of Pascal's triangle
+
+    Return: list of lists of integers representing a Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(0, n):
+        row = [1] * (i + 1)
+
+        for j in range(0, len(row):
+            if i == 0 or i == n - 1:
+                row[j] = 1
+            else:
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
