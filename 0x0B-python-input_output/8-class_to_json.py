bb27ba007a34af977a68bd53ba6652e73deccb13
LiantsoaANDR <liantsoa.cpro@gmail.com>
2023-12-05 10:40:35 -0800

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,6 +9,6 @@ def class_to_json(obj):
     (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
-    if hasattr(obj, "__dict__")
+    if hasattr(obj, "__dict__"):
         return obj.__dict__
     return ({})
