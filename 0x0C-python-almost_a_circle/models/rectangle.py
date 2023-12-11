905ca3ebe675f8ebbac2097e6bc0b71738992cdd
Liantsoa <liantsoa.cpro@gmail.com>
2023-12-11 06:18:49 -0800

    Task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,3 +110,27 @@ class Rectangle(Base):
         """Returns the characteristic of the rectangle"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format
                 (self.id, self.__x, self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """
+        update the rectangle
+
+        Args:
+            1st: id attribute
+            2nd: width attribute
+            3rd: height attribute
+            4th: x attribute
+            5th: y attribute
+        """
+        if not args:
+            return
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
