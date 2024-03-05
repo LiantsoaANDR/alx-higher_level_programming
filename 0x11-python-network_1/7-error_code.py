3b0c656f2d0ee58ab108fd870273bf77302ba6f5
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-03-05 09:57:43 -0800

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL, displays the body of the response
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code,
+by using the packages requests and sys
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
