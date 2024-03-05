6c134565b07a25f66020f06b46d1f8bdd2fadb81
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-03-05 11:01:44 -0800

    Pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module of the function find_peak"""
 
+
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers."""
     if not list_of_integers:
